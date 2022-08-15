b31a1057bce6b81a66ce03403d7208d3f15027b0
Hridoy <ridoyking9999@gmail.com>
2022-08-15 13:46:44 +0600

    Completed some basic C++

---
+++ Competetive C++/02_c++_basic.cpp
@@ -0,0 +1,8 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+	cout << "Hello " << "My name is EliteLearner";
+	cout << "00" << sqrt(49);
+
+}
---
+++ Competetive C++/03_variables.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+	// int a = 5;
+	// int b = 10;
+	// int sum = a + b;
+	// cout << "The sum is " << sum <<endl;
+
+	     //DATA TYPES...
+//char, int, float, double, bool;
+	char c = 'a';
+	int a = 8;
+	double d = 5.8;
+	a = 8.9;
+	cout << c << a << d << endl; 
+
+}
