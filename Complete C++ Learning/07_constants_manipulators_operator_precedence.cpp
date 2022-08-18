135c788536cd5641ffaaa2187a91919f7fef278c
Hridoy <ridoyking9999@gmail.com>
2022-08-18 22:30:52 +0600

    Inline Functions, Default Arguments & Constant Arguments in C++

--- Complete C++ Learning/07_constants_manipulators_operator_precedence.cpp
+++ Complete C++ Learning/07_constants_manipulators_operator_precedence.cpp
@@ -17,22 +17,22 @@ int main(){
     //***************Manupulators in C++:**********
 
 
-    // int a = 3, b = 422, c = 4456;
-    // cout << "The value of a without setw is : " << a << endl;
-    // cout << "The value of b without setw is : " << b << endl;
-    // cout << "The value of c without setw is : " << c << endl;
+    int a = 3, b = 422, c = 4456;
+    cout << "The value of a without setw is : " << a << endl;
+    cout << "The value of b without setw is : " << b << endl;
+    cout << "The value of c without setw is : " << c << endl;
 
-    // cout << "The value of a is : " << setw(4) << a << endl;
-    // cout << "The value of b is : " << setw(4) << b << endl;
-    // cout << "The value of c is : " << setw(4) << c << endl;
+    cout << "The value of a is : " << setw(4) << a << endl;
+    cout << "The value of b is : " << setw(4) << b << endl;
+    cout << "The value of c is : " << setw(4) << c << endl;
 
     
     //***************Operator Precedence************
 
-    int a = 3, b = 4;
-    //int d = (a * 5) + b;
-    int c = ((((a * 5)+b)-45)+87);
-    cout << c;
+    // int a = 3, b = 4;
+    // //int d = (a * 5) + b;
+    // int c = ((((a * 5)+b)-45)+87);
+    // cout << c;
 
     return 0;
 }
