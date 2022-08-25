61f3f636627e3460323e2a5814d52fe3ae454573
Hridoy <ridoyking9999@gmail.com>
2022-08-25 12:26:59 +0600

    Learnt some new things about C++ syntax

---
+++ C++ Practices/Week - 01/Module - 02/2_4_How_to_Take _input_in_C++.cpp
@@ -0,0 +1,18 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    // int num1, num2;
+    // cin >> num1 >> num2;
+    // cout << num1 <<" "<< num2; 
+
+    //For string:
+    char str[50];
+    //cin >> str; -->This will not get the whole string, after whitespace it stops.
+    cin.getline(str, 50);
+    cout << str;
+    
+    //For array:
+    //same as C...
+    return 0;
+}
---
+++ C++ Practices/Week - 01/Module - 02/2_5_typecasting.cpp
@@ -0,0 +1,12 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    double a = 2.44553;
+
+    //Like as C...
+    int x = (double) a;
+    //Both are Currect but 2nd is not valid for C.
+    // int x = double (a);
+    return 0;
+}
